bc1600547143883ae95a26b24262682ac12fe176
Evan McPheron <evan.mcpheron@icloud.com>
2022-07-17 16:06:23 -0700

    working on file directory

--- src/app/pages/Home/HomeConfig.js
+++ src/app/pages/Home/HomeConfig.js
@@ -24,28 +24,3 @@ const HomeConfig = {
 };
 
 export default HomeConfig;
-
-/**
- * Lazy load Example
- */
-/*
-import React from 'react';
-
-const Example = lazy(() => import('./Example'));
-
-const ExampleConfig = {
-  settings: {
-    layout: {
-      config: {},
-    },
-  },
-  routes: [
-    {
-      path: 'example',
-      element: <Example />,
-    },
-  ],
-};
-
-export default ExampleConfig;
-*/
